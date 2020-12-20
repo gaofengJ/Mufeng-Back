e32ee9c1c97e4cd03a9c4770f7017e195cb9c0c4
gaofengJ <760613910@qq.com>
2020-12-20 12:34:20 +0800

    修改远程数据库配置

--- conf/db.js
+++ conf/db.js
@@ -1,14 +1,13 @@
 const fs = require('fs')
+const path = require('path')
 const env = process.env.NODE_ENV // 环境参数：根据package中执行脚本判断
 
 const configPathMap = {
   dev: '../../mufeng-back-db-config.json',
-  production: ''
+  production: '../../mufeng-back-db-config.json'
 }
 
-const { MYSQL_CONF, REDIS_CONF } = JSON.parse(fs.readFileSync(configPathMap[env], 'utf8'))
-
-console.log(MYSQL_CONF, REDIS_CONF)
+const { MYSQL_CONF, REDIS_CONF } = JSON.parse(fs.readFileSync(path.join(__dirname, configPathMap[env]), 'utf8'))
 
 module.exports = {
   MYSQL_CONF,
