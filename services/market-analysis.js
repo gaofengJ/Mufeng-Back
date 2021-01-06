d0ac4fa687a683f55d921d18eb0e9a1bdb82231b
gaofeng_jin <gaofeng_jin@intsig.net>
2021-01-06 21:08:57 +0800

    [RC]个股涨跌统计功能

--- services/market-analysis.js
+++ services/market-analysis.js
@@ -21,43 +21,43 @@ async function getPercentStatistics (date) {
   date = utils._dateFormat(date, 'yyyyMMdd')
   let rangeArr = [
     {
-      key: '<-9',
+      key: '<-9%',
       value: 'AND pct_chg <= -9'
     },
     {
-      key: '-9~-8',
+      key: '-9%~-8%',
       value: 'AND -9 <= pct_chg AND pct_chg < -8'
     },
     {
-      key: '-8~-7',
+      key: '-8%~-7%',
       value: 'AND -8 <= pct_chg AND pct_chg < -7'
     },
     {
-      key: '-7~-6',
+      key: '-7%~-6%',
       value: 'AND -7 <= pct_chg AND pct_chg < -6'
     },
     {
-      key: '-6~-5',
+      key: '-6%~-5%',
       value: 'AND -6 <= pct_chg AND pct_chg < -5'
     },
     {
-      key: '-5~-4',
+      key: '-5%~-4%',
       value: 'AND -5 <= pct_chg AND pct_chg < -4'
     },
     {
-      key: '-4~-3',
+      key: '-4%~-3%',
       value: 'AND -4 <= pct_chg AND pct_chg < -3'
     },
     {
-      key: '-3~-2',
+      key: '-3%~-2%',
       value: 'AND -3 <= pct_chg AND pct_chg < -2'
     },
     {
-      key: '-2~-1',
+      key: '-2%~-1%',
       value: 'AND -2 <= pct_chg AND pct_chg < -1'
     },
     {
-      key: '-1~0',
+      key: '-1%~0',
       value: 'AND -1 <= pct_chg AND pct_chg < 0'
     },
     {
@@ -65,23 +65,23 @@ async function getPercentStatistics (date) {
       value: 'AND pct_chg = 0'
     },
     {
-      key: '0~1',
+      key: '0~1%',
       value: 'AND 0 < pct_chg AND pct_chg <= 1'
     },
     {
-      key: '1~2',
+      key: '1%~2%',
       value: 'AND 1 < pct_chg AND pct_chg <= 2'
     },
     {
-      key: '2~3',
+      key: '2%~3%',
       value: 'AND 2 < pct_chg AND pct_chg <= 3'
     },
     {
-      key: '3~4',
+      key: '3%~4%',
       value: 'AND 3 < pct_chg AND pct_chg <= 4'
     },
     {
-      key: '4~5',
+      key: '4%~5%',
       value: 'AND 4 < pct_chg AND pct_chg <= 5'
     },
     {
@@ -89,19 +89,19 @@ async function getPercentStatistics (date) {
       value: 'AND 5 < pct_chg AND pct_chg <= 6'
     },
     {
-      key: '6~7',
+      key: '6%~7%',
       value: 'AND 6 < pct_chg AND pct_chg <= 7'
     },
     {
-      key: '7~8',
+      key: '7%~8%',
       value: 'AND 7 < pct_chg AND pct_chg <= 8'
     },
     {
-      key: '8~9',
+      key: '8%~9%',
       value: 'AND 8 < pct_chg AND pct_chg <= 9'
     },
     {
-      key: '>9',
+      key: '>9%',
       value: 'AND 9 < pct_chg'
     },
   ]
